be87caf85397cad44191174d9e8fb872c9505870
Facundo Sa <facundo.sa@web.com>
2019-05-23 17:03:26 -0300

    Routing module added and routing features treated

--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-home',
+  //selector: 'pm-home', Remove this because selector is only needed when we want to use it as a nested component
   templateUrl: './welcome.component.html'
 })
 export class WelcomeComponent {
